74b5b52894409e3d969c3b9d17ef65dea5950e61
Daniel Taskoff <daniel.taskoff@gmail.com>
2014-03-08 16:50:00 +0200

    generate_numbers script and an empty tests file added.

---
+++ week0/generate_numbers/solution.py
@@ -0,0 +1,22 @@
+# a script that generates 'n' numbers in some range
+# and writes them in a file with the name 'filename'
+import sys
+from random import randint
+
+def generate_numbers(filename, n):
+	file = open(filename, "w")
+	for i in range(0, n):
+		file.write(str(randint(1, 1000)) + " ")
+	file.close()
+
+def main():
+	args = sys.argv
+	if len(args) < 3:
+		print("no filename or number passed")
+	else:
+		filename = sys.argv[1]
+		n = int(sys.argv[2])
+		generate_numbers(filename, n)
+
+if __name__ == '__main__':
+    main()
