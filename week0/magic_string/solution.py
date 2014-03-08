b035a9ee50ef26a2b6d09329ceb8e435b7067f88
Daniel Taskoff <daniel.taskoff@gmail.com>
2014-03-08 14:44:52 +0200

    magic_string script and an empty tests file added.

---
+++ week0/magic_string/solution.py
@@ -0,0 +1,12 @@
+# solution to:
+# http://community.topcoder.com/stat?c=problem_statement&pm=13004&rd=15842
+def magic_string(string):
+	count = 0
+	middle = len(string) // 2
+	for char in string[:middle]:
+		if char == '<':
+			count += 1
+	for char in string[middle:]:
+		if char == '>':
+			count += 1
+	return count
