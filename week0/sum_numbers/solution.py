63d73ed44bcf601fef3354e7b9e0a4334c60de46
Daniel Taskoff <daniel.taskoff@gmail.com>
2014-03-08 16:50:00 +0200

    sum_numbers script and an empty tests file added.

---
+++ week0/sum_numbers/solution.py
@@ -0,0 +1,24 @@
+# a script that sums all numbers in a file
+# with name 'filename' (containing only numbers)
+import sys
+
+def sum_numbers(filename):
+	file = open(filename, "r")
+	numbers = file.read().split()
+	file.close()
+
+	sum = 0
+	for number in numbers:
+		sum += int(number)
+	return sum
+
+def main():
+	args = sys.argv
+	if len(args) < 2:
+		print("no filename passed")
+	else:
+		filename = sys.argv[1]
+		print(sum_numbers(filename))
+
+if __name__ == '__main__':
+    main()
