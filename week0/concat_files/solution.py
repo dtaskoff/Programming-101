da38776995506a11e6c290b6525a710f36a352a3
Daniel Taskoff <daniel.taskoff@gmail.com>
2014-03-08 16:50:00 +0200

    concat_files script and an empty tests file added.

---
+++ week0/concat_files/solution.py
@@ -0,0 +1,24 @@
+# a script that opens the files 'filenames'
+# and writes their content in MEGATRON
+import sys
+
+def concat_files(filenames):
+	MEGATRON = open("MEGATRON", "a")
+
+	for filename in filenames:
+		file = open(filename, "r")
+		MEGATRON.write(file.read())
+		MEGATRON.write("\n\n")
+		file.close()
+	MEGATRON.close()
+
+def main():
+	args = sys.argv
+	if len(args) < 2:
+		print("no filenames passed")
+	else:
+		filenames = sys.argv[1:]
+		concat_files(filenames)
+
+if __name__ == '__main__':
+	main()
