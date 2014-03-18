671b26c3c56e20c70d6d5dc4df118d7cced12b62
Daniel Taskoff <daniel.taskoff@gmail.com>
2014-03-18 13:59:25 +0200

    pizza_delivery script and test added

--- week0/pizza_delivery/solution.py
+++ week0/pizza_delivery/solution.py
@@ -4,122 +4,127 @@ from glob import glob
 from datetime import datetime
 
 def pizza_delivery():
-	order = {}
-	listed = False
-	saved = True
+    order = {}
+    listed = False
+    saved = True
 
-	while True:
-		command = input("Enter command: ")
-		commands = command.split()
-		cmd = commands[0]
+    while True:
+        command = input("Enter command: ")
+        commands = command.split()
+        cmd = commands[0]
 
-		if cmd == "take":
-			if len(commands) < 3:
-				unknown()
-			else:
-				take(commands[1], float(commands[2]), order)
-				saved = False
-		elif cmd == "status":
-			status(order)
-		elif cmd == "save":
-			save(order)
-			listed = False
-			saved = True
-		elif cmd == "list":
-			list()
-			listed = True
-		elif cmd == "load":
-			if listed and saved:
-				order = load(int(commands[1]), order)
-				listed = False
-			elif not saved:
-				print("You have not saved the current order.")
-				print("If you wish to discard it, type load <number> again.")
-				saved = True
-			else:
-				print("Use list command before loading")
-		elif cmd == "finish":
-			if not saved:
-				print("You have not saved your order.")
-				print("If you wish to continue, type finish again.")
-				print("If you want to save your order, type save")
-				saved = True
-			else:
-				print("Finishing order. Bye!")
-				break
-		elif cmd == "help":
-			help()
-		else:
-			unknown()
+        if cmd == "take":
+            if len(commands) < 3:
+                unknown()
+            else:
+                print(take(commands[1], float(commands[2]), order))
+                saved = False
+        elif cmd == "status":
+            print('\n'.join(status(order)))
+        elif cmd == "save":
+            print(save(order))
+            listed = False
+            saved = True
+        elif cmd == "list":
+            print('\n'.join(list_()))
+            listed = True
+        elif cmd == "load":
+            if listed and saved:
+                order = load(int(commands[1]), order)
+                listed = False
+            elif not saved:
+                print("You have not saved the current order.")
+                print("If you wish to discard it, type load <number> again.")
+                saved = True
+            else:
+                print("Use list command before loading")
+        elif cmd == "finish":
+            if not saved:
+                print("You have not saved your order.")
+                print("If you wish to continue, type finish again.")
+                print("If you want to save your order, type save")
+                saved = True
+            else:
+                print("Finishing order. Bye!")
+                break
+        elif cmd == "help":
+            help()
+        else:
+            unknown()
 
 def take(name, price, order):
-	if name in order:
-		order[name] += price
-	else:
-		order[name] = price
-	print("Taking order from %s for %.2f"%(name, price))
+    order[name] = order.setdefault(name, price)
+
+    return "Taking order from %s for %.2f"%(name, price)
 
 def status(order):
-	for name in order:
-		format_output(name, order[name])
+    status = []
+
+    for name in order:
+        status.append(format_output(name, order[name]))
+
+    return status
 
 def save(order):
-	ts = time()
-	stamp = datetime.fromtimestamp(ts).strftime('%Y_%m_%d_%H_%M_%S')
-	file = open('orders_%s'%stamp, "w")
+    ts = time()
+    stamp = datetime.fromtimestamp(ts).strftime('%Y_%m_%d_%H_%M_%S')
+    file_ = open('orders_%s'%stamp, "w")
 
-	for name in order:
-		file.write(name + " - " + str(order[name]) + "\n")
-	file.close()
+    for name in order:
+        file_.write(name + " - " + str(order[name]) + "\n")
+    file_.close()
 
-	print("Order saved")
+    return "Order saved"
 
-def list():
-	order_files = glob("orders_????_??_??_??_??_??")
+def list_():
+    order_files = glob("orders_????_??_??_??_??_??")
 
-	for index in range(0, len(order_files)):
-		format_output("[%d]"%(index + 1), order_files[index])
+    list_ = []
+    for index in range(0, len(order_files)):
+        list_.append(format_output("[%d]"%(index + 1), order_files[index]))
+
+    return list_
 
 def load(number, order):
-	order_files = glob("orders_????_??_??_??_??_??")
-	file_to_load = order_files[number - 1]
+    order_files = glob("orders_????_??_??_??_??_??")
+    file_to_load = order_files[number - 1]
 
-	file = open(file_to_load, "r")
-	content = file.read()
-	file.close()
+    file_ = open(file_to_load, "r")
+    content = file_.read()
+    file_.close()
 
-	all_orders = content.splitlines()
-	all_orders = map(lambda string: string.split(" - "), all_orders)
+    all_orders = content.splitlines()
+    all_orders = map(lambda string: string.split(" - "), all_orders)
 
-	order = {}
+    order = {}
 
-	for single_order in all_orders:
-		order[single_order[0]] = float(single_order[1])
+    for single_order in all_orders:
+        order[single_order[0]] = float(single_order[1])
 
-	return order
+    return order
 
 def help():
-	print("List of possible commands: ")
-	print("take <name> <price>")
-	print("status")
-	print("save")
-	print("list")
-	print("load <number>")
-	print("finish")
-	print("help")
+     return "List of possible commands: \n"\
+     + "take <name> <price>\n"\
+     + "status\n"\
+     + "save\n"\
+     + "list\n"\
+     + "load <number>\n"\
+     + "finish\n"\
+     + "help\n"
 
 def unknown():
-	print("Unknown command.")
-	print("Try entering help for further information.")
+    return "Unknown command.\n"\
+        + "Try entering help for further information."
 
 def format_output(fst, snd):
-	if type(snd) == type(0.00):
-		print("%s - %.2f"%(fst, snd))
-	else:
-		print("%s - %s"%(fst, snd))
+    if type(snd) == type(0.00):
+        return "%s - %.2f"%(fst, snd)
+    else:
+        return "%s - %s"%(fst, snd)
 
 def main():
-	pizza_delivery()
+    pizza_delivery()
 
 if __name__ == '__main__':
-	main()
+    main()
--- week0/pizza_delivery/tests.py
+++ week0/pizza_delivery/tests.py
@@ -1,1 +1,54 @@
 # I'm not testing that..
+import unittest
+import solution
+
+
+class TestPizzaDelivery (unittest.TestCase):
+    def setUp(self):
+        self.order = {"initial": 0.00}
+
+    def test_take(self):
+        result = solution.take("tester", 42.00, self.order)
+        self.assertEqual("Taking order from tester for 42.00", result)
+        self.assertEqual({"initial": 0.00, "tester": 42.00}, self.order)
+
+    def test_status(self):
+        result = solution.status(self.order)
+        self.assertEqual(["initial - 0.00"], result)
+
+    def test_format_output(self):
+        result = solution.format_output("first", "second")
+        self.assertEqual("first - second", result)
+
+    #def test_save(self):
+    #    result = solution.save(self.order)
+    #    self.assertEqual("Order saved", result)
+
+    def test_list(self):
+        self.assertEqual(1, len(solution.list_()))
+
+    def test_load(self):
+        file_ = open("orders_0000_00_00_00_00_00", "w")
+        file_.write("tester - 42.00")
+        file_.close()
+        result = solution.load(1, self.order)
+        self.assertEqual({"tester": 42.00}, result)
+
+    def test_help(self):
+        self.assertEqual("List of possible commands: \n"\
+            + "take <name> <price>\n"\
+            + "status\n"\
+            + "save\n"\
+            + "list\n"\
+            + "load <number>\n"\
+            + "finish\n"\
+            + "help\n",
+            solution.help())
+
+    def test_unknown(self):
+        self.assertEqual("Unknown command.\n"\
+            + "Try entering help for further information.",
+            solution.unknown())
+
+if __name__ == '__main__':
+    unittest.main()
