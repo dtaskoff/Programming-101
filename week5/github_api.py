0cc5d4951bc26b45f8fa03582c8f9723b0fec121
Daniel Taskoff <daniel.taskoff@gmail.com>
2014-04-19 22:56:47 +0300

    printing stats for repo almost done, there's need to edit line counting

--- week5/github_api.py
+++ week5/github_api.py
@@ -1,6 +1,8 @@
 import requests
 import config
 import zipfile
+import os
+from collections import defaultdict
 
 
 def request(url):
@@ -35,20 +37,73 @@ def download_repos(repos_names, user):
 
 def unzip_repos(repos_names):
     for repo in repos_names:
-        with zipfile.ZipFile("{}.zip".format(repo)) as myzip:
-            myzip.extractall()
+        zipfile.ZipFile("{}.zip".format(repo)).extractall()
 
-def get_repos_stats(repos_names):
-    pass
+def get_extension(file):
+    index = len(file)
+    for i in range(len(file)):
+        if file[i] == '.':
+            index = i
+            break
+    return file[index:]
+
+def print_extensions(extensions):
+    to_print = []
+    for extension in extensions:
+        to_print.append("\"*{0}\" - {1} files".format(extension,
+            extensions[extension]))
+    return "\n".join(to_print)
+
+def get_repos_stats(repo):
+    extensions = defaultdict(int)
+    list_files = os.walk(repo)
+
+    for root, dirs, paths in list_files:
+        for path in paths:
+            extension = str(get_extension(path))
+            extensions[extension] += 1
+
+    return extensions
+
+def get_lines(path):
+    file = open(path, "r")
+    length = len(file.read().splitlines())
+    file.close()
+
+    return length
+
+# TODO
+def lines_of_code_in_repo(repo):
+    lines = 0
+    list_files = os.walk(repo)
+
+    for dirs in list_files:
+        dir = dirs[0]
+        files = dirs[2]
+        for file in files:
+            lines += get_lines(dir + '/' + file)
+
+    return "{} lines".format(lines)
+
+def all_repos_stats(repos_names):
+    stats = []
+
+    for repo in repos_names:
+        repo_full_name = "{}-master".format(repo)
+        stats.append(repo)
+        stats.append(str(lines_of_code_in_repo(repo_full_name)))
+        stats.append(print_extensions(get_repos_stats(repo_full_name)))
+
+    return '\n'.join(stats)
+
 
 def main():
-    # user = "dtaskoff"
-    # repos_url = get_repos_url("user")
-    # repos_names = fetch_repos(repos_url)
-    # download_repos(repos_names, "user")
-    # unzip_repos(repos_names)
-    # get_repos_stats(repos_names)
-    pass
+    user = "dtaskoff"
+    repos_url = get_repos_url(user)
+    repos_names = fetch_repos(repos_url)
+    download_repos(repos_names, user)
+    unzip_repos(repos_names)
+    print(all_repos_stats(repos_names))
 
 
 if __name__ == '__main__':
--- week5/test_github_api.py
+++ week5/test_github_api.py
@@ -1,6 +1,7 @@
 import unittest
 import github_api
 import zipfile
+import os
 from subprocess import call
 
 # only tests for the zipfile part are written
@@ -8,17 +9,35 @@ from subprocess import call
 class TestGithubAPI(unittest.TestCase):
     def setUp(self):
         self.test_zip = zipfile.ZipFile("test.zip", "w")
-        file = open("test", "w")
+        file = open("test.tst", "w")
         file.write("just a simple test\non two lines")
         file.close()
-        self.test_zip.write("test")
+        self.test_zip.write("test.tst")
 
     def test_unzip_repos(self):
+        self.test_zip.close()
         github_api.unzip_repos(["test"])
+        self.assertTrue(os.path.exists("test.tst"))
+
+    def test_get_extension(self):
+        result = github_api.get_extension("test.tst")
+        self.assertEqual(".tst", result)
+
+    def test_print_extensions(self):
+        expected = "\"*.tst\" - 1 files"
+        result = github_api.print_extensions({".tst": 1})
+        self.assertEqual(expected, result)
+
+    def test_get_repos_stats(self):
+        result = github_api.get_repos_stats(".")
+        self.assertTrue(".tst" in result)
+
+    def test_get_lines(self):
+        self.assertEqual(2, github_api.get_lines("test.tst"))
 
     def tearDown(self):
         self.test_zip.close()
-        call("rm -r test", shell=True)
+        call("rm -r test.tst", shell=True)
         call("rm -r test.zip", shell=True)
 
 if __name__ == '__main__':
