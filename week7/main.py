1bb9a3cc557c0dec17b69b986b2bda802c7d0787
Daniel Taskoff <daniel.taskoff@gmail.com>
2014-04-22 21:49:05 +0300

    some newlines added for pretty looking

--- week7/main.py
+++ week7/main.py
@@ -1,8 +1,10 @@
 from game import MathGame
 
+
 def main():
     math_game = MathGame()
     math_game.loop()
+
 
 if __name__ == '__main__':
     main()
--- week7/scoreboard.py
+++ week7/scoreboard.py
@@ -6,6 +6,7 @@ from sqlalchemy import desc
 
 Base = declarative_base()
 
+
 class Player(Base):
     __tablename__ = "players"
     position = Column(Integer, primary_key = True)
@@ -15,6 +16,7 @@ class Player(Base):
     def __str__(self):
         return "{}. {} with {} points".format(self.position,
             self.name, self.points)
+
 
 class Scoreboard():
     def __init__(self):
